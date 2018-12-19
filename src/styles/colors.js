0bc2edf5f4b0ea3a2fa69b7b7534be875fdd42e8
carlosviana <viana.mail@gmail.com>
2018-12-19 13:09:27 -0200

    Ajustando layout padrao

---
+++ src/styles/colors.js
@@ -0,0 +1,17 @@
+export default {
+  white: '#FFF',
+  lighter: '#EEE',
+  light: '#DDD',
+  regular: '#999',
+  dark: '#666',
+  black: '#000',
+
+  primary: '#7A91CA',
+  secundary: '#444A5A',
+  success: '#9DCA83',
+  danger: '#E37A7A',
+
+  transparent: 'transparent',
+  darktransparent: 'rgba(0,0,0,0.6)',
+  whitetransparent: 'rgba(255,255,255,0.3)',
+};
